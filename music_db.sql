3ecd48c88b8af3920c5a9a76af1e37bef6ee86a3
Ancy <ancy.annajay@gmail.com>
2021-04-03 13:21:21 -0400

    Created database and made joins

--- music_db.sql
+++ music_db.sql
@@ -1,46 +1,43 @@
--- Creating tables for Final Challenge
 CREATE TABLE member_details (
-     Customer_ID VARCHAR(30) NOT NULL,
-     City VARCHAR(2) NOT NULL,
-	 Age VARCHAR(2) NOT NULL,
-	 Gender VARCHAR(6) NOT NULL,
-	 Registered_Via VARCHAR(2) NOT NULL,
-	 Registered_On VARCHAR(8) NOT NULL,
-     PRIMARY KEY (Gender),
-     UNIQUE (Customer_ID)
+     Customer_ID VARCHAR(75),
+	 Age VARCHAR(5),
+	 Gender VARCHAR(6),
+	 PRIMARY KEY (Customer_ID)
+	);
+
+CREATE TABLE transactions (
+     Customer_ID VARCHAR(75),
+	 Payment_Plan_Days VARCHAR(10),
+	 Auto_Renewed VARCHAR(10),
+	 Cancelled VARCHAR(1)
+);
+	
+		   
+CREATE TABLE user_logs (
+     Customer_ID VARCHAR(75),
+	 Less_than_25 VARCHAR(4),
+	 Above_99 VARCHAR(4),
+	 Unique_Songs VARCHAR(4),
+	 Total_Secs VARCHAR(25)
 );
 
 CREATE TABLE train_dataset (
-     Customer_ID VARCHAR(30) NOT NULL,
-     Is_Churn VARCHAR(1) NOT NULL,
-     PRIMARY KEY (Is_Churn),
-     UNIQUE (Customer_ID)
+     Customer_ID VARCHAR(75),
+     Is_Churn VARCHAR(1)
 );
 
-CREATE TABLE transactions (
-     Customer_ID VARCHAR(30) NOT NULL,
-     Payment_Method_ID VARCHAR(2) NOT NULL,
-	 Payment_Plan_Days VARCHAR(2) NOT NULL,
-	 Plan_List_Price VARCHAR(3) NOT NULL,
-	 Actual_Amount_Paid VARCHAR(3) NOT NULL,
-	 Auto_Renewed VARCHAR(1) NOT NULL,
-	 Transaction_Date VARCHAR(8) NOT NULL,
-	 Membership_Expire_Date VARCHAR(8) NOT NULL,
-	 Cancelled VARCHAR(1) NOT NULL,
-	 PRIMARY KEY (Payment_Method_ID),
-     UNIQUE (Customer_ID)
-);
 
-CREATE TABLE user_logs (
-     Customer_ID VARCHAR(30) NOT NULL,
-     Date_Streamed VARCHAR(8) NOT NULL,
-	 Less_than_25 VARCHAR(4) NOT NULL,
-	 Less_than_50 VARCHAR(4) NOT NULL,
-	 Less_than_75 VARCHAR(4) NOT NULL,
-	 Less_than_99 VARCHAR(4) NOT NULL,
-	 Above_99 VARCHAR(4) NOT NULL,
-	 Unique_Songs VARCHAR(4) NOT NULL,
-	 Total_Secs VARCHAR(10) NOT NULL,
-     PRIMARY KEY (Unique_Songs),
-     UNIQUE (Customer_ID)
-);
+--Joining Member Initial Details Dataset with Train Dataset
+SELECT member_details.Customer_ID, member_details.Gender, member_details.Age, train_dataset.Is_Churn
+FROM member_details
+INNER JOIN train_dataset ON member_details.Customer_ID = train_dataset.Customer_ID;
+
+--Joining Transactions Dataset with Train Dataset
+SELECT transactions.Customer_ID, transactions.Payment_Plan_Days, transactions.Auto_Renewed, transactions.Cancelled, train_dataset.Is_Churn
+FROM transactions
+INNER JOIN train_dataset ON transactions.Customer_ID = train_dataset.Customer_ID;
+
+--Joining User Logs Dataset with Train Dataset
+SELECT user_logs.Customer_ID, user_logs.Less_than_25, user_logs.Above_99, user_logs.Unique_Songs, user_logs.Total_Secs, train_dataset.Is_Churn
+FROM user_logs
+INNER JOIN train_dataset ON user_logs.Customer_ID = train_dataset.Customer_ID;
